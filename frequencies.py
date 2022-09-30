82e592234d9770b0ee940438d6e275660f31b146
DvinHartoonian <dvin.hartoonian@gmail.com>
2022-09-30 12:50:59 +0100

    Add functionality to count elements of a list and store the frequency in a dictionary

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,21 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    incomplete = True
+
+    if len(items) == 0:
+        return frequencies
+
+    while incomplete:
+        count = 0
+        item = items[0]
+        for i in list(items):
+            if str(item) == str(i):
+                count += 1
+                items.remove(i)
+
+        frequencies[str(item)] = count
+        if len(items) == 0:
+            incomplete = False
+
     return frequencies
